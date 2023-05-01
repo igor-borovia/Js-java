3bd218fb077f066b48f2444af54b954205c18e3e
igor-borovia <123493551+igor-borovia@users.noreply.github.com>
2023-05-01 22:29:20 +0300

    2 завдання зробив

---
+++ JS/main.js
@@ -0,0 +1,47 @@
+let hello = `Hello`
+console.log(hello)
+let owu = `owu`
+    console.log(owu)
+let com = `com`
+console.log(com)
+let ua = `ua`
+console.log(ua)
+let a = `-999`
+console.log(a)
+let b = `1`
+console.log(b)
+let c = `10`
+console.log(c)
+let d = `123`
+console.log(d)
+let i = `3,14`
+console.log(i)
+let f = `2,7`
+console.log(f)
+let g = `16`
+console.log(g)
+let h = `troe`
+console.log(h)
+let k = `folse`
+console.log(k)
+
+// 2 завдання,
+let firstName = `igor`
+let middleName = `borovia`
+let lastName = `sergioyvith`
+console.log(firstName, middleName, lastName, )
+ // завдання 3
+// let a1 = `100`
+// let b1 = `100`
+// let c1= `true`
+// console.log( typeof )
+
+
+
+
+
+
+
+
+
+
