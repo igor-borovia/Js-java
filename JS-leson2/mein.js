2f66104d7413c6feb73fd9f6beed1438c0fe8184
igor-borovia <123493551+igor-borovia@users.noreply.github.com>
2023-05-03 22:56:46 +0300

    book

---
+++ JS-leson2/mein.js
@@ -0,0 +1,28 @@
+let arr = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+];
+console.log(arr)
+// ------------------------------
+let users = {
+    title: `tomas-shelbi`,
+    pageCount: `avto`,
+    genre: `300`,
+}
+console.log(users)
+//---------------------------------
+let book = {
+    title: `taras-ivanov`,
+    pageCount: `it-techno`,
+    genre: 3000,
+    authors: [`igor`,`taras`,`andy`],
+    wife: {
+       name1: `igor`,
+       age: 20,
+       name2: `taras`,
+        age2: 30,
+        name3: `andy`,
+        age3: 44,
+    }
+}
+console.log(book)
+//--------------------------------------
